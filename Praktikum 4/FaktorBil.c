77b9f42f48093361c68b624b5770a63686fb5e0e
RadityaIH <100180125+RadityaIH@users.noreply.github.com>
2022-03-15 21:35:38 +0700

    Add files via upload

---
+++ Praktikum 4/FaktorBil.c
@@ -0,0 +1,29 @@
+///*Nama File : FaktorBil.c */
+///*Deskripsi : Membuat algoritma untuk menentukan faktor bilangan dari sembarang integer*/
+///*Pembuat : Raditya Ilham Hastoro - 24060121140125*/
+///*Tanggal : Selasa, 15 Maret 2022 17:28 WIB*/
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+///*Kamus*/
+//    int N, i;
+//
+///*Algoritma*/
+//    printf("Masukkan bilangan : ");
+//    scanf("%d", &N);
+//
+//    if (N>0) {
+//        for (i=1; i<N; i++) {
+//            if (N%i == 0) {
+//                printf("%d, ", i);
+//            }
+//        }
+//        printf("dan %d", i);
+//    }
+//    else {
+//        printf("Masukkan bilangan positif!");
+//    }
+//    return 0;
+//}
